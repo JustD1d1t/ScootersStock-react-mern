301121e5cd21ec70b09f8e05bb86a340c027c751
JustD1d1t <starzyk.dawid.0826@gmail.com>
2023-05-16 10:35:58 +0200

    fix links

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,7 @@
 // };
 export const config = {
   url: "https://binance-backend.herokuapp.com/",
-  userUrl: "https://binance-backend.herokuapp.com//scooters-user",
-  scootersUrl: "https://binance-backend.herokuapp.com//scooters",
-  orderUrl: "https://binance-backend.herokuapp.com//order",
+  userUrl: "https://binance-backend.herokuapp.com/scooters-user",
+  scootersUrl: "https://binance-backend.herokuapp.com/scooters",
+  orderUrl: "https://binance-backend.herokuapp.com/order",
 };
